83d17f14657f89713b0b663606fa2cb64c18eeb2
Taras Yuzkiv <yuzkv.taras1111@gmail.com>
2023-09-25 16:16:17 +0300

    Added solution and MySql DB

---
+++ ListApi/Data/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using ListApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListApi.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
